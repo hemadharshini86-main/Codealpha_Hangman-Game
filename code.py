0847b4882c69d230e25c84a47f6d4fcf5394d7af
hemadharshini86-main <hemadharshini86@gmail.com>
2026-06-19 08:54:02 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,15 +1,6 @@
-
-# Hangman Game in Python
-
 import random
-
-# Predefined list of words
 words = ["infosys", "apple", "cognizant", "oracle", "microsoft"]
-
-# Randomly choose a word
 secret_word = random.choice(words)
-
-# Variables
 guessed_letters = []
 incorrect_guesses = 0
 max_incorrect = 6
@@ -18,11 +9,7 @@ print("Welcome to Hangman Game!")
 print("Guess the word one letter at a time.")
 print("You have 6 incorrect guesses.\n")
 print("Hint: Name of Famous Companies !!!")
-
-# Game loop
 while incorrect_guesses < max_incorrect:
-
-    # Display the word with blanks
     display_word = ""
 
     for letter in secret_word:
@@ -33,28 +20,21 @@ while incorrect_guesses < max_incorrect:
 
     print("Word:", display_word)
 
-    # Check if player guessed the word
     if "_" not in display_word:
         print("\n🎉 Congratulations! You guessed the word:", secret_word)
         break
 
-    # User input
     guess = input("Enter a letter: ").lower()
 
-    # Validate input
     if len(guess) != 1 or not guess.isalpha():
         print(" Please enter only one alphabet letter.\n")
         continue
-
-    # Check repeated guess
     if guess in guessed_letters:
         print("You already guessed that letter.\n")
         continue
 
-    # Add guess to list
     guessed_letters.append(guess)
 
-    # Check correct or incorrect
     if guess in secret_word:
         print(" Correct guess!\n")
     else:
@@ -63,7 +43,6 @@ while incorrect_guesses < max_incorrect:
         print(" Wrong guess! ")
         print("Remaining incorrect guesses:", remaining, "\n")
 
-# If player loses
 if incorrect_guesses == max_incorrect:
     print(" Game Over!")
     print("The correct word was:", secret_word)
